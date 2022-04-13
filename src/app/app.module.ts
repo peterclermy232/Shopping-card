1ea3b717f9267f4d60e99758944ef1dc6a75584b
peterclermy <peterclermy232@gmail.com>
2022-04-13 23:41:11 +0300

    shopping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FilterPipe } from './shared/filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    CartComponent,
+    ProductsComponent,
+    HeaderComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
